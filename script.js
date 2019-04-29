368da997813e712681603f7090322de80dfead8f
AndrezaSantana <44169394+AndrezaSantana@users.noreply.github.com>
2019-04-29 18:40:57 -0300

    function - calculate
    Addition of the function that calculates the IMC

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function submit(){
-   alert(document.getElementById('peso').value);
+   var a = document.getElementById('peso').value;
+   var b = document.getElementById('altura').value;
+   
+    function imc(a,b){
+    result = a/Math.pow(b, 2)*10000;
+    var arredondado = parseFloat(result.toFixed(2));
+    return arredondado;
+    }
+    alert(imc(a,b));
 }
+
+//validar formulário
+//exibir o resultado no corpo da página
