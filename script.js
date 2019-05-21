cf4b094f0bd4a33655a1fe25ba5e5ae2f4e91918
AndrezaSantana <44169394+AndrezaSantana@users.noreply.github.com>
2019-05-21 14:04:08 -0300

    New Commit used JQuery

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
-function submit(){
-   var a = document.getElementById('peso').value;
-   var b = document.getElementById('altura').value;
-   
-    function imc(a,b){
-    result = a/Math.pow(b, 2)*10000;
-    var arredondado = parseFloat(result.toFixed(2));
-    return arredondado;
+function submit() {
+    var weight = $('#weight').val();
+    var height = $('#height').val();
+
+    var calc = weight / Math.pow(height, 2) * 10000;
+    var result = parseFloat(calc.toFixed(2));
+    
+    $('#imc').html(result);
+
+    if (result <= 16.9) {
+        $('#classification').html("Muito abaixo do peso");
+        $('#range').html("IMC: 0 a 16,9");
+        $('#description').html("Sintomas: Queda de cabelo, infertilidade, ausência menstrual");
     }
-    alert(imc(a,b));
+    else if (result <= 18.4) {
+        $('#classification').html("Abaixo do peso");
+        $('#range').html("IMC: 17 a 18,4");
+        $('#description').html("Sintomas: Fadiga, stress, ansiedade");
+    }
+    else if (result <= 24.9) {
+        $('#classification').html("Peso normal");
+        $('#range').html("IMC: 18,5 a 24,9");
+        $('#description').html("Sintomas: Menor risco de doenças cardíacas e vasculares");
+    }
+    else if (result <= 29.9) {
+        $('#classification').html("Acima do peso");
+        $('#range').html("IMC: 25 a 29,9");
+        $('#description').html("Sintomas: Fadiga, má circulação, varizes");
+    }
+    else if (result <= 34.9) {
+        $('#classification').html("Obesidade Grau I");
+        $('#range').html("IMC: 30 a 34,9");
+        $('#description').html("Sintomas: Diabetes, angina, infarto, aterosclerose");
+    }
+     else if (result <= 40) {
+        $('#classification').html("Obesidade Grau II");
+        $('#range').html("IMC: 35 a 40");
+        $('#description').html("Sintomas: Apneia do sono, falta de ar");
+    }
+     else {
+        $('#classification').html("Obesidade Grau III");
+        $('#range').html("IMC: maior que 40");
+        $('#description').html("Sintomas: Refluxo, dificuldade para se mover, escaras, diabetes, infarto, AVC");
+    }
+    
+    $('.result').show();
+    $('html, body').animate({scrollTop:$(document).height()}, 'slow');
 }
 
-//validar formulário
-//exibir o resultado no corpo da página
+$( '#clear' ).click(function() {
+  $( '.result').hide();
+})
